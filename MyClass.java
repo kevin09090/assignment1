3277bda08f17b4b573c75792eb70bc8d54fdfb79
unknown <haha030921@gmail.com>
2020-09-02 15:48:09 -0700

    assignment1

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(Stringp[] args) {
+        char a = 63, b = 23, c = 23;
+        System.out.prinln(a);
+        System.out.prinln(b);
+        System.out.prinln(c);
+    }
+}
---
+++ MyClass1.java
@@ -0,0 +1,9 @@
+public class MyClass1 {
+    public static void main(String[] args ){
+        int MyInt = 9;
+        double myDouble = MyInt; // << Windening casting (int to double)
+    
+        System.out.println(myDouble);
+
+    }
+}
